9dddb9a5c3752bb23ea0eab9558e1c65521432a6
daliduraku <mesherbetmolla@gmail.com>
2024-10-02 20:47:24 -0400

    Created the add, subtract, multiply, divide and operation functions

---
+++ myScript.js
@@ -0,0 +1,33 @@
+
+function add(num1, num2){
+    return num1 + num2
+}
+
+function subtract(num1, num2){
+    return num1 - num2
+}
+
+function multiply(num1, num2){
+    return num1 * num2
+}
+
+function divide(num1, num2){
+    return num1 / num2
+}
+
+function operate(operator, num1, num2){
+
+    if ( operator === "+"){
+        result = add(num1, num2);
+        return result
+    }else if (operator === "-"){
+        result = subtract(num1, num2);
+        return result
+    }else if (operator === "*"){
+        result = multiply(num1, num2);
+        return result
+    }else if (operator === "/"){
+        result = divide(num1, num2);
+        return result
+    }
+}

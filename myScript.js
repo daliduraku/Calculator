8428d1d09313088a4176c8cca77a7bf11e898204
daliduraku <mesherbetmolla@gmail.com>
2024-10-03 23:15:16 -0400

    Added the clear button and resets the display to be clear. Added the equal button to evaluate the expression entered

--- myScript.js
+++ myScript.js
@@ -1,41 +1,64 @@
 
-function add(num1, num2){
-    return num1 + num2
-}
+function operate(str) {
+    let operator = "";
+    let numArr = [];
+    let num = "";
 
-function subtract(num1, num2){
-    return num1 - num2
-}
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
 
-function multiply(num1, num2){
-    return num1 * num2
-}
-
-function divide(num1, num2){
-    return num1 / num2
-}
-
-function operate(operator, num1, num2){
-
-    if ( operator === "+"){
-        result = add(num1, num2);
-        return result
-    }else if (operator === "-"){
-        result = subtract(num1, num2);
-        return result
-    }else if (operator === "*"){
-        result = multiply(num1, num2);
-        return result
-    }else if (operator === "/"){
-        result = divide(num1, num2);
-        return result
+        if (char === "+" || char === "-" || char === "*" || char === "/") {
+            operator = char;
+            numArr.push(parseFloat(num));  // Push the current number into the array
+            num = "";  // Reset for the next number
+        } else {
+            num += char;  // Build the number as a string
+        }
     }
+
+    // Push the last number into the array
+    if (num !== "") {
+        numArr.push(parseFloat(num));
+    }
+
+    let result;
+    if (operator === "+") {
+        result = numArr.reduce((a, b) => a + b);
+    } else if (operator === "-") {
+        result = numArr.reduce((a, b) => a - b);
+    } else if (operator === "*") {
+        result = numArr.reduce((a, b) => a * b);
+    } else if (operator === "/") {
+        result = numArr.reduce((a, b) => a / b);
+    }
+
+    return result;
 }
 
-const buttons = document.querySelectorAll('.btn-number, .btn-operator');
-const display = document.querySelector('.display');
 
-numberBtn.addEventListener('click', function(){
-    let num1 = numberBtn
+const buttons = document.querySelectorAll('.btn-number, .btn-operator')
+const display = document.querySelector('.display')
+let displayData = "";
 
+buttons.forEach(button =>{
+    button.addEventListener('click', () =>{
+        const buttonValue = button.getAttribute('data-num');
+        
+        displayData += buttonValue;
+        display.textContent = displayData;
+    })
+})
+
+const equalsButton = document.querySelector('.btn-equals')
+
+equalsButton.addEventListener('click', function(){
+    displayData = operate(displayData)
+    display.textContent = displayData
+} )
+
+const clearButton = document.querySelector(".btn-clear")
+
+clearButton.addEventListener('click', () => {
+    displayData = "";
+    display.textContent = displayData;
 })
